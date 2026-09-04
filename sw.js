7fd65cb27c749998dd377b5e082a0cc048a465f2
NikoloziGagua <niku.gagua@gmail.com>
2026-09-04 05:14:29 +0400

    Redesign mobile capture experience

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='combat-notes-v3';
+const CACHE='combat-notes-v4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
